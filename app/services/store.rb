aa28ae6a5355de47898a4ae628831e330fb518f9
Marshall Houston <marshall.houston@gmail.com>
2018-01-02 11:17:02 -0700

    add initialize method for stores, passing store spec

--- app/services/store.rb
+++ app/services/store.rb
@@ -1,3 +1,12 @@
 class Store
-  
+  attr_reader :name, :city, :distance, :phone_number, :store_type
+
+  def initialize(attrs)
+    @name = attrs[:longName]
+    @city = attrs[:city]
+    @distance = attrs[:distance]
+    @phone_number = attrs[:phone]
+    @store_type = attrs[:storeType]
+  end
+
 end
