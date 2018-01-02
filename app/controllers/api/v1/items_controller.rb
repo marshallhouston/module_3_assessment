e04c8f5c8d1ecacff7c8706559a7910b0888b3a2
Marshall Houston <marshall.houston@gmail.com>
2018-01-02 09:47:26 -0700

    add request spec for delete item. status code is 200, not 204. trying to debug

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ItemsController < Api::V1::BaseController
-  before_action :set_item, only: :show
+  before_action :set_item, only: [:show, :destroy]
 
   def index
     render json: Item.all
@@ -7,6 +7,16 @@ class Api::V1::ItemsController < Api::V1::BaseController
 
   def show
     render json: @item
+  end
+
+  def destroy
+    if @item
+      @item.destroy
+      render json: { status: 204 }
+      # render json: { head: :no_content }
+    else
+      render json: {status: :not_found}
+    end
   end
 
   private
---
+++ spec/requests/api/v1/delete_an_item_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+# When I send a DELETE request to `/api/v1/items/1`
+# I receive a 204 JSON response if the record is successfully deleted
+
+describe "Items API" do
+  describe "delete /api/v1/items/:id" do
+    it "returns a 204 JSON response if the record is successfully deleted" do
+      item = create(:item)
+
+      header = { 'CONTENT_TYPE':'application/json' }
+
+      expect {
+        delete "/api/v1/items/#{item.id}", headers: header
+      }.to change(Item, :count).by(-1)
+
+      require "pry"; binding.pry
+      expect(response.status).to eq 204
+    end
+
+    xit "returns a 404 JSON response if the record is not successfully deleted" do
+      header = { 'CONTENT_TYPE':'application/json' }
+
+      delete "/api/v1/items/2", headers: header
+
+      expect(response.status).to eq 404
+    end
+  end
+end
